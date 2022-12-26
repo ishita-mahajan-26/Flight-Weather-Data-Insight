1641902d96cd566dd90e03b958b5583502ee76e9
ishita-mahajan-26 <121415896+ishita-mahajan-26@users.noreply.github.com>
2022-12-26 06:18:22 +0100

    Update sql_queries.py

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -16,12 +16,18 @@ class SqlQueries:
             ELSE True 
             END,
         CASE f.Diverted WHEN 0.00 THEN False ELSE True END,
-        w.location , w.time , w.maxtempC , w.mintempC , w.tempC , w.weather_Desc 
+        w.time , 
+        w.maxtempC , 
+        w.mintempC , 
+        w.tempC , 
+        w.weather_Desc 
         from 
         flight as f 
         JOIN
         weather as w 
-        ON f.FlightDate = w.date AND SUBSTRING(f.CRSDepTime, 1, 2) = SUBSTRING(w.time, 1, 2)
+        ON f.FlightDate = w.date 
+        AND SUBSTRING(f.CRSDepTime, 1, 2) = SUBSTRING(w.time, 1, 2)
+        and w.location = f.Origin
     """)
 
     weather_table_insert = ("""
@@ -69,49 +75,18 @@ class SqlQueries:
 
     flight_table_insert = ("""
            SELECT DISTINCT  
-           md5( TailNum|| CRSArrTime) flightId, FlightDate, TailNum, FlightNum, Origin, Dest, CRSDepTime, CRSArrTime, ArrTime, ArrDelay, Cancelled, WeatherDelay, Diverted 
-            FROM
-            staging_flights
-    """)
-    
-    songplay_table_insert = ("""
-        SELECT
-                md5(events.sessionid || events.start_time) songplay_id,
-                events.start_time, 
-                events.userid, 
-                events.level, 
-                songs.song_id, 
-                songs.artist_id, 
-                events.sessionid, 
-                events.location, 
-                events.useragent
-                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
-            FROM staging_events
-            WHERE page='NextSong') events
-            LEFT JOIN staging_songs songs
-            ON events.song = songs.title
-                AND events.artist = songs.artist_name
-                AND events.length = songs.duration
-    """)
-
-    user_table_insert = ("""
-        SELECT distinct userid, firstname, lastname, gender, level
-        FROM staging_events
-        WHERE page='NextSong'
-    """)
-
-    song_table_insert = ("""
-        SELECT distinct song_id, title, artist_id, year, duration
-        FROM staging_songs
-    """)
-
-    artist_table_insert = ("""
-        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
-        FROM staging_songs
-    """)
-
-    time_table_insert = ("""
-        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
-               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
-        FROM songplays
+           md5( TailNum|| CRSArrTime) flightId, 
+           FlightDate, 
+           TailNum, 
+           FlightNum, 
+           Origin, 
+           Dest, 
+           CRSDepTime, 
+           CRSArrTime, 
+           ArrTime, 
+           ArrDelay, 
+           Cancelled, 
+           WeatherDelay, 
+           Diverted 
+           FROM staging_flights
     """)
